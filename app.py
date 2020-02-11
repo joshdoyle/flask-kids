042a57e8fc299e3105f4b52bba299d409ff66ab7
Josh Doyle <me@joshdoyle.us>
2020-02-11 16:59:32 -0600

    model created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 DEBUG = True
 PORT = 8000
@@ -8,6 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return 'Hello, kids!'
+
+@app.route('/say_hello/<username>')
+def say_hello(username): # this func takes URL param as arg
+  return "Hello {}".format(username)	
 
 if __name__ == '__main__':
 	app.run(debug=DEBUG, port=PORT)
---
+++ models.py
@@ -0,0 +1,22 @@
+from peewee import *
+
+DATABASE = SqliteDatabase('kids.sqlite')
+
+class Kid(Model):
+	name = CharField()
+	age = Integer()
+	gender = CharField()
+	created_at = DateTimeField(default=datetime.datetime.now)
+
+
+	class Meta:
+		database = DATABASE
+
+def initialize(): 
+  	DATABASE.connect() 
+  	DATABASE.create_tables([Kid], safe=True)
+  	print("Connected to DB and created tables if they weren't already there")
+
+  	DATABASE.close()
+
+
